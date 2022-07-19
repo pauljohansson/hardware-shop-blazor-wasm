5f998117e3d5665b3243b741ec4829620759d155
tywald <pralinen@gmail.com>
2022-07-19 08:27:32 +0200

    prepare HardwareShopDbContext class

---
+++ HardwareShop.Api/Data/HardwareShopDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace HardwareShop.Api.Data
+{
+    public class HardwareShopDbContext:DbContext
+    {
+        public HardwareShopDbContext(DbContextOptions<HardwareShopDbContext> options):base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}

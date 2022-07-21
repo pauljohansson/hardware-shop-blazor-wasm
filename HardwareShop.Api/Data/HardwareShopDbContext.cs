88a1268ff372570ffa756555252a1e2ee03e3926
tywald <pralinen@gmail.com>
2022-07-21 07:25:15 +0200

    add public properties for the entities

--- HardwareShop.Api/Data/HardwareShopDbContext.cs
+++ HardwareShop.Api/Data/HardwareShopDbContext.cs
@@ -542,5 +542,11 @@ namespace HardwareShop.Api.Data
 				Name = "Cooling"
 			});
 		}
+
+        public DbSet<Cart> Carts { get; set; }
+        public DbSet<CartItem> CartItems { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductCategory> ProductCategories { get; set; }
+        public DbSet<User> Users { get; set; }
     }
 }

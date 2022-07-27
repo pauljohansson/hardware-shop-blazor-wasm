a55c53a19087540624bc5f84ecf13679ffd39407
tywald <pralinen@gmail.com>
2022-07-27 07:57:38 +0200

    add ProductRepository class

---
+++ HardwareShop.Api/Repositories/ProductRepository.cs
@@ -0,0 +1,28 @@
+﻿using HardwareShop.Api.Entities;
+using HardwareShop.Api.Repositories.Contracts;
+
+namespace HardwareShop.Api.Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        public Task<IEnumerable<ProductCategory>> GetCategories()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ProductCategory> GetCategory(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Product> GetItem(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Product>> GetItems()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

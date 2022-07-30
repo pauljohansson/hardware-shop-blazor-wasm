65f6db704bff231be27167a6ee139caa4d9bdd83
tywald <pralinen@gmail.com>
2022-07-30 08:32:59 +0200

    partially implement GetItems method

--- HardwareShop.Api/Controllers/ProductController.cs
+++ HardwareShop.Api/Controllers/ProductController.cs
@@ -1,4 +1,5 @@
 ﻿using HardwareShop.Api.Repositories.Contracts;
+using HardwareShop.Models.Dtos;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -13,6 +14,30 @@ namespace HardwareShop.Api.Controllers
         public ProductController(IProductRepository productRepository)
         {
             this.productRepository = productRepository;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ProductDto>>> GetItems()
+        {
+            try
+            {
+                var products = await this.productRepository.GetItems();
+                var productCategories = await this.productRepository.GetCategories();
+
+                if (products == null || productCategories == null)
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
         }
     }
 }

27bc28cdf173d40a85e584d541a8aa83c560167d
tywald <pralinen@gmail.com>
2022-08-09 07:53:13 +0200

    register ProductService for dependency injection

--- HardwareShop.Web/Program.cs
+++ HardwareShop.Web/Program.cs
@@ -1,4 +1,6 @@
 using HardwareShop.Web;
+using HardwareShop.Web.Services;
+using HardwareShop.Web.Services.Contracts;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -7,5 +9,7 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7294/") });
+
+builder.Services.AddScoped<IProductService, ProductService>();
 
 await builder.Build().RunAsync();

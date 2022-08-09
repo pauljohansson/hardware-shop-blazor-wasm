724b7aedad35a888c248f33a5f4b2c5a1126e717
tywald <pralinen@gmail.com>
2022-08-09 07:52:12 +0200

    add Products razor component and make it inherit ProductsBase class

---
+++ HardwareShop.Web/Pages/ProductsBase.cs
@@ -0,0 +1,11 @@
+﻿using HardwareShop.Web.Services.Contracts;
+using Microsoft.AspNetCore.Components;
+
+namespace HardwareShop.Web.Pages
+{
+    public class ProductsBase:ComponentBase
+    {
+        [Inject]
+        public IProductService ProductService { get; set; }
+    }
+}

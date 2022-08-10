813b0261aec8027cc709ee71e65d6357223b6427
tywald <pralinen@gmail.com>
2022-08-10 07:50:34 +0200

    move 'display products' into its own child razor component

---
+++ HardwareShop.Web/Pages/DisplayProductsBase.cs
@@ -0,0 +1,11 @@
+﻿using HardwareShop.Models.Dtos;
+using Microsoft.AspNetCore.Components;
+
+namespace HardwareShop.Web.Pages
+{
+    public class DisplayProductsBase:ComponentBase
+    {
+        [Parameter]
+        public IEnumerable<ProductDto> Products { get; set; }
+    }
+}
